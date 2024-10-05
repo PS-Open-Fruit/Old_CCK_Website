ef96cc6352f81c1ec5cbbe7d2155993dae3730ca
SkyJTx <SkyJTx@users.noreply.github.com>
2024-10-05 04:22:24 +0000

    deploy: 6be43f5988dab77caa6427828292fefa5ba9308f

---
+++ about.php
@@ -0,0 +1,130 @@
+<?php
+    
+    ?>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><!-- Start of headline -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<link rel="icon" href="./images/icon.jpg" type="image/jpg" />
+<title>ชมรมคมพิวเตอร์ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ</title>
+
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+
+<script type="text/javascript" src="js/jquery-1.6.3.min.js" ></script>
+<script type="text/javascript" src="js/jquery-ui.min.js" ></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#featured > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
+	});
+</script>
+
+<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+<!-- import java script -->
+<script type="text/javascript" src="js/ddsmoothmenu.js"> 
+</script>
+
+<script type="text/javascript">
+
+ddsmoothmenu.init({
+	mainmenuid: "templatemo_menu", //menu DIV id
+	orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+	classname: 'ddsmoothmenu', //class added to menu's outer DIV
+	//customtheme: ["#1c5a80", "#18374a"],
+	contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+})
+
+</script>
+
+<link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" /> 
+<script type="text/JavaScript" src="js/slimbox2.js"></script> 
+
+</head>
+<body>
+<div id="templatemo_wrapper">
+	<div id="templatemo_header"><span class="header_border"></span>
+            <div id="site_title"><a><img src="./images/icon.png" witth="50" height="50" align="center"/>ชมรมคอมฯ</a></div>
+		<div id="templatemo_menu" class="ddsmoothmenu">
+            <ul>
+                <li><a href="index.php" class="selected"><span></span>Home</a></li>
+                <li><a href="members.php"><span></span>Members</a>
+                    <ul>
+                        <li><a href="signup.php">Sign up</a></li>
+                       
+                  </ul>
+                </li>
+                <li><a href="activities.php"><span></span>Activities</a>
+
+                </li>
+                <li><a href="about.php"><span></span>About</a></li>
+                
+                <li><a href="contact.php"><span></span>Contact</a></li>
+            </ul>
+            <br style="clear: left" />
+        </div> <!-- end of templatemo_menu -->
+        <div class="clear"></div>
+    </div> <!-- END of header -->
+    
+    <div id="templatemo_main_content">
+    	<div id="templatemo_sidebar">
+            <div class="sidebar_content_box">
+            	<h3>ลิงค์ที่เกี่ยวข้อง</h3>
+                <ul class="sidebar_link">
+                    <li><a href="http://www.kmutnb.ac.th">KMUTNB</a></li>
+                    <li><a href="klogic.kmutnb.ac.th:8080/kris/index.jsp">ระบบลงทะเบียน</a></li>
+                    <li><a href="https://mail.kmutnb.ac.th/login">เมลล์นักศึกษา</a></li>
+                    <li><a href="http://regis.kmutnb.ac.th/data_doc/calendar/intro_calendar.html">ปฏิทินการศึกษา</a></li>
+                    <li><a href="http://mermaid.eng.kmutnb.ac.th/exam/dispseatexam/">เลขที่นั่งสอบ</a></li>
+                </ul>
+            </div>
+
+        </div>
+        
+        <div id="templatemo_content">
+        	<div class="content_wrapper content_mb_60">
+            <div class="col_2">
+                <h2>ประวัติชมรมคอมพิวเตอร์</h2>
+                <p><em> ชมรมคอมพิวเตอร์ก่อตั้งขึ้นเมื่อปี พ.ศ. 25xx โดยการนำของนักศึกษาภาควิชาวิทยาการคอมพิวเตอร์และเทคโนโลยีสารสนเทศชั้นปีที่ x ในสมัยนั้น </em></p>
+                <ul class="list_bullet">
+                    <li>"รักเรียน รักกิจกรรม รักคอมพิวเตอร์"</li>
+                </ul>
+			</div>
+     
+     ชมรมคอมพิวเตอร์ได้รับการสนับสนุนจากภาควิชาวิทยาการคอมพิวเตอร์และเทคโนโลยีสารสนเทศ รวมถึงจากทางคณะวิทยาศาสตร์ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือเป็นอย่างดี
+     โดยการจัดตั้งครั้งแรกนั้นมีวัตถุประสงค์เพื่อจัดตั้งเป็นชมรมทั่วไปในการเป็นสถานที่นัดแนะ ชี้แจงกิจกรรมต่างๆ รวมถึงการจัดสอนพิเศษในรายวิชาต่างๆของภาควิชาในกลุ่มสมาชิกชมรมด้วยกันเอง
+     จนกระทั่งชมรมคอมพิวเตอร์ได้ดำเนินงานเรื่อยมาจนถึงปัจจุบันได้มีการจัดกิจกรรมต่างๆมากมายทั้งออกค่ายอาสา การจัดสอนพิเศษ การจัดค่ายรับน้อง ซึ่งล้วนแล้วแต่เป็นประโยชน์ต่อการใช้ชีวิตในมหาวิทยาลัยแทบทั้งสิ้น   
+     ดังนั้น ในปัจจุบันนี้จึงมีการสนองเจตารมณ์ดังกล่าวในการสร้างชมรมที่มีประโยชน์ต่อนิสิตของมหาวิทยาลัยแห่งนี้ โดยการจัดกิจกรรมร่วมกันให้มีรูปธรรมขึ้น มีแบบแผนแน่นอน มีการจัดการที่มีประสิทธิภาพ  
+     มีคณะบุคคลเข้ามาบริหารกิจการของชมรมให้มีความเป็นระบบ และสามารถดำเนินงานได้อย่างถูกต้อง  ชมรมคอมพิวเตอร์นั้นได้ก่อตั้งมา xx ปีแล้ว 
+     จนถึงปัจจุบันยังแน่แน่วต่อการสร้างสรรค์ และทำงานเพื่อสังคม มาโดยตลอด ซึ่งเราจะนำเทคโนโลยีก้าวไปพร้อมกับความพัฒนาดังปณิธานของชมรม 
+
+ <BR /><BR /><BR />
+            <div class="col_2 no_margin_right">
+                <h2>วัตถุประสงค์ชมรม</h2>
+           	  <p><em>
+                          1.    เสริมสร้างทักษะการเรียนรู้ทางด้านคอมพิวเตอร์<BR />
+
+2.    เพื่อเผยแพร่ความรู้และประสบการณ์ให้แก่บุคคลที่สนใจ<BR />
+
+3.    เพื่อสืบต่อเจตนารมณ์ของผู้ก่อตั้งชมรมคอมพิวเตอร์<BR />
+
+4.    เพื่อส่งเสริมการทำกิจกรรมนอกเวลาเรียน<BR />
+
+5.    เพื่อฝึกทักษะด้านการคิด วิเคราะห์ วางแผน และการทำงานเป็นหมู่คณะ</em></p>
+            </div>	
+ <div class="col_3 no_margin_right"><img src="./images/icon.png"></div>
+        </div>
+            
+        
+        </div> <!-- END of templatemo_content -->
+        <div class="clear"></div>
+    </div> <!-- END of templatemo_main_content -->
+    
+<div id="templatemo_footer">
+    	Copyright © 2014 CompterClub KMTNB | Design 
+    </div> <!-- END of templatemo_footer -->
+</div> <!-- END of templatemo_wrapper -->
+</body>
+<script type='text/javascript' src='js/logging.js'></script>
+</html>
